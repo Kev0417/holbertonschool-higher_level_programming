efa152dd7f51078d5210581fce5288a5bdd14dfc
Kev0417 <1681@holbertonschool.com>
2020-06-03 21:33:16 -0500

    Edit solution for 0x0B 6

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+"""
+This module contains a function that returns
+an object (Python data structure)
+represented by a JSON string
+"""
 import json
-"""
-This file contains a function that
-returns an object represented by a
-JSON string
-"""
 
 
 def from_json_string(my_str):
-    """
-    function to return object
+    """function that returns
+    an object (Python data structure)
     represented by a JSON string
+    Returns:
+        obj: Python data structure
     """
     return (json.loads(my_str))
