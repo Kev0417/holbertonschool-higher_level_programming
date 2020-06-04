b101cffae9ae3c8a5491a8f8ecea4b7149439e73
Kev0417 <1681@holbertonschool.com>
2020-06-03 21:34:09 -0500

    Edit solution for 0x0B 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-import json
-from sys import argv
 """
 This file contains a function that adds
 all arguments to a python list and saves
 to a file
 """
+import sys
 
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
-
-filename = 'add_item.json'
-
+filename = "add_item.json"
 try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
+    new_file = load_from_json_file(filename)
+except Exception:
+    new_file = []
 
-new = new + argv[1:]
-save_json(new, filename)
+new_file = new_file + sys.argv[1:]
+save_to_json_file(new_file, filename)
