504331cba2309537a079699405e27048da5ac3fa
Kev0417 <1681@holbertonschool.com>
2020-06-03 21:24:05 -0500

    Edit solution for 0x0B 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """
-This file contains function that appends
-a string at the end of a text file and
-returns number of characters added
-"""
+	This module contains a function that
+writes a string to a text file (UTF8)
+	and returns the number of characters written
+	"""
 
 
-def append_write(filename="", text=""):
-with open(filename, mode="a", encoding="utf-8") as a_file:
-return (a_file.write(str(text)))
+	def append_write(filename="", text=""):
+	"""function that
+writes a string to a text file (UTF8)
+	and returns the number of characters written
+	Args:
+	filename (str, optional): path to file. Defaults to "".
+							  text (str, optional): text to write. Defaults to "".
+													"""
+													   with open(filename, encoding="utf-8", mode="a") as a_file:
+																	  return (a_file.write(text))
