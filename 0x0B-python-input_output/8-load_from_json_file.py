bb423bdbdcb4bf1adcc0ca9e86968556edd0c5a0
Kev0417 <1681@holbertonschool.com>
2020-06-03 21:33:51 -0500

    Edit solution for 0x0B 8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contains a function
+that creates an Object from a “JSON file
+"""
 import json
-"""
-This file contains a function
-that creates an object from a "JSON FILE"
-"""
 
 
 def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file
+    Returns:
+        obj: Python data structure
     """
-    function that creates obj from json file
-    """
-    with open(filename) as myFile:
-        new_obj = json.load(myFile)
-    return (new_obj)
+    with open(filename) as write_file:
+        my_obj = json.load(write_file)
+    return my_obj
